0a56c121aeceb3a9ea66d0f5ff6d503560fc8e19
Philip Porto Schiffer <philip.schiffer@sprylab.com>
2026-03-05 13:00:04 +0100

    Fix region for cookies_private_key

--- modules/lambdas/cookies.tf
+++ modules/lambdas/cookies.tf
@@ -4,10 +4,10 @@ resource "tls_private_key" "lambda" {
 }
 
 resource "aws_ssm_parameter" "cookies_private_key" {
-  provider = aws.us-east-1
-  name     = "/purple/cloudfront/lambda/purple-web-${var.bucket_name}/privateKey"
-  type     = "SecureString"
-  value    = tls_private_key.lambda.private_key_pem
+  region = "us-east-1"
+  name   = "/purple/cloudfront/lambda/purple-web-${var.bucket_name}/privateKey"
+  type   = "SecureString"
+  value  = tls_private_key.lambda.private_key_pem
 }
 
 resource "aws_cloudfront_public_key" "lambda" {
@@ -20,10 +20,10 @@ resource "aws_cloudfront_public_key" "lambda" {
 }
 
 resource "aws_ssm_parameter" "cookies_keypair_id" {
-  provider = aws.us-east-1
-  name     = "/purple/cloudfront/lambda/purple-web-${var.bucket_name}/keyPairId"
-  type     = "String"
-  value    = aws_cloudfront_public_key.lambda.id
+  region = "us-east-1"
+  name   = "/purple/cloudfront/lambda/purple-web-${var.bucket_name}/keyPairId"
+  type   = "String"
+  value  = aws_cloudfront_public_key.lambda.id
 }
 
 data "aws_iam_policy_document" "web_signed_cookies" {
