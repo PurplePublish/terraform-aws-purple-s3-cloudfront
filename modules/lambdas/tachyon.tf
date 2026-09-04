1a5ec7c7d380425dd59facd531470f60a8679873
Philip Porto Schiffer <philip.schiffer@sprylab.com>
2026-09-04 09:38:05 +0200

    Update tachyon lambda

--- modules/lambdas/tachyon.tf
+++ modules/lambdas/tachyon.tf
@@ -41,7 +41,7 @@ module "tachyon" {
   lambda_at_edge                    = true
   publish                           = true
   create_package                    = false
-  local_existing_package            = "${path.module}/lambda/tachyon/tachyon-r48.zip"
+  local_existing_package            = "${path.module}/lambda/tachyon/tachyon-r51.zip"
   cloudwatch_logs_retention_in_days = 30
   attach_policy_jsons               = true
   number_of_policy_jsons            = 1
